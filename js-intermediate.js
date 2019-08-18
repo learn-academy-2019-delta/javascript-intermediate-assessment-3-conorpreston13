5557e46c052fdad9d63cc54930403f3a3cce85ab
Ubuntu <ubuntu@ip-172-31-28-134.us-east-2.compute.internal>
2019-08-18 16:11:26 +0000

    initial commmit

--- js-intermediate.js
+++ js-intermediate.js
@@ -4,8 +4,17 @@
 // Be sure to use all given test cases
 
 // 1. Write a function called getFib that returns the first 10 numbers of the Fibonacci sequence in an array. Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-
+let findFib = function (num) {
+  if (num === 1){
+    return [0, 1];
+  } 
+  else {
+    let newNum = findFib(num - 1);
+    newNum.push(newNum[newNum.length - 1] + newNum[newNum.length - 2]);
+    return newNum;
+  }
+}
+ console.log(findFib(10));
 
 // 2. Write a function called oddChecker that takes in an array and returns a new array of only odd numbers.
 
@@ -13,8 +22,19 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [9, 7, 199, -9]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [7, 23, -823]
+let oddChecker = (arr) => {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 !== 0){
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
+console.log(oddChecker(fullArr1))
+console.log(oddChecker(fullArr2))
 
-
+// i do not know for sure how to only return numbers. maybe with typeOf()? If so, i do not know where to call that method.
 
 // 3. Given the object below, complete the console.log to find specific information:
 
@@ -29,13 +49,13 @@ var bicycle = {
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log the bell:
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the PSI:
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 
 // 4. Write a function that takes one argument of type array and returns the contents of that array in reverse order.
@@ -44,18 +64,26 @@ var originalArray1 = [1, 2, 3, 4, 5, 6, 7]
 // Expected output: [7, 6, 5, 4, 3, 2, 1],
 var originalArray2 = ["a", "t", "l", "e", "d"]
 // Expected output: ["d", "e", "l", "t", "a"]
-
-
+let reverseArr = (arrType) => {
+  return arrType.reverse()
+}
+console.log(reverseArr(originalArray1))
+console.log(reverseArr(originalArray2))
 
 // 5. Write the code that would make this test pass.
 
-describe("multTwo", () => {
-    test ("returns an array with all the numbers multiplied by two", () => {
-        expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
-        expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
-        expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
-    })
-})
+// describe("multTwo", () => {
+//     test ("returns an array with all the numbers multiplied by two", () => {
+//         expect(multTwo([3, 4, 5])).toEqual([6, 8, 10])
+//         expect(multTwo([23, -9, 0])).toEqual([46, -18, 0])
+//         expect(multTwo([4.5, -4.5, 12])).toEqual([9, -9, 24])
+//     })
+// })
+// let multTwo = (arr) => {
+//   let splArr = ...arr
+//   return splArr * 2
+// }
+// console.log(multTwo([3, 4, 5]))
 
 
 
@@ -63,22 +91,36 @@ describe("multTwo", () => {
 
 var ourString = "Hello Learn Students!"
 // Expected output: 3
-
-
+let findL = (str) => {
+  let newLArr = []
+  let newStr = str.toLowerCase().split('')
+    for (let i = 0; i < str.length; i++)
+    if(newStr[i] === 'l'){
+       newLArr.push(newStr[i])
+    }
+    return newLArr.length;
+}
+console.log(findL(ourString))
 
 // 7. Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
 
 var middleLetters1 = "hello"
 // Expected output: “l”
-var middleLetters2 = "lamas"
+var middleLetters2 = "llamas"
 // Expected output: “am”
-
+let findMiddle = (letters) => {
+  if(letters.length % 2 !== 0){
+    
+  } else {
+    
+  }
+}
 
 
 
 // 8. Write a program to get the area of a sphere using object classes. Create three spheres with different radi as test cases. Area of a sphere =  4πr^2 (four pi r squared)
 
-
+// i am havin trouble starting this program
 
 
 // 9. Use object destructuring to return the nested values of specs. Expected output: 4, "manual"
@@ -92,8 +134,8 @@ var myCar = {
     transmission: "manual",
   }
 }
-
-
+let { doors, transmission } = myCar.specs
+console.log(doors, transmission)
 
 // 10. Stretch: Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
--- js-intermediate.md
+++ js-intermediate.md
@@ -5,36 +5,36 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. What are the HTTP verbs? Which CRUD action corresponds to each verb?
 
-  Your answer:
+  Your answer: get, delete, put
 
-  Researched answer:
+  Researched answer: post (create), get(read), put/patch(update), delete(delete)
 
 
 2. Why would you use object destructuring?
 
-  Your answer:
+  Your answer: to avoid using dot notation in every line of code
 
-  Researched answer:
+  Researched answer: estructuring assignment is a special syntax that allows us to “unpack” arrays or objects into a bunch of variables, as sometimes that's more convenient.
 
 
 
 3. Why is testing important?
 
-  Your answer:
+  Your answer: it is easier to manage all of your inputted code rather than getting a bug in 100 lines 
 
-  Researched answer:
+  Researched answer: It helps to detect bugs and helps in releasing a stable application. Also testing helps in early detection of bugs before merging it to production
 
 
 4. What is the difference between a class and an object?
 
-  Your answer:
+  Your answer: objects do not hold behavier, classes are the marriage of objects and behavior
 
-  Researched answer:
+  Researched answer: a class can be considered as a construct which encapsulates a group of variables and methods; whereas, an object acts as member or instance of that class. 
 
 
 5. What did you learn during the group project this week? Please include any additional feedback you may have.
 
-  Your answer:
+  Your answer: I learned the basics of react and how to pair program better
 
 
 
@@ -42,57 +42,56 @@ Answer the following questions. First, without external resources. Challenge you
 
 1. How do you link a CSS file to your HTML page?
 
-  Your answer:
+  Your answer: you import it using script?
 
-  Researched answer:
+  Researched answer: Href attribute specifies the location of the file which you need to create a link with. 
 
 
 2. What is the difference between a div and a span?
 
-  Your answer:
+  Your answer: i do not know what a span is
 
-  Researched answer:
+  Researched answer:  a span element is in-line and usually used for a small chunk of HTML inside a line (such as inside a paragraph) whereas a div (division) element is block-line (which is basically equivalent to having a line-break before and after it) and used to group larger chunks of code.
 
 
 3. What is a CSS class? When should you use an id instead of a class?
 
-  Your answer:
+  Your answer: like a class in react, you can use it mutiple times
 
-  Researched answer:
-
+  Researched answer: Use a CSS Class to Style an Element. Classes are reusable styles that can be added to HTML elements. 
 
 4. Name 4 semantic HTML tags.
 
-  Your answer:
+  Your answer: div, h1 (all h tags)
 
-  Researched answer:
+  Researched answer: abbr, acronym, blockquote, dfn
 
 
 5. What are three options for creating responsive design?
 
-  Your answer:
+  Your answer: flexible images, adjusting screen resolution, touchscreens/cursors
 
-  Researched answer:
+  Researched answer: flexible images, adjusting screen resolution, touchscreens/cursors
 
 
 ### Stretch: The following questions are potential interview questions. First, try to answer each question on your own then Google the answer to further your knowledge.
 
 1. What is front end development? Can you identify any tools/skills that are uniquely required of front end developers?
 
-  Your answer:
+  Your answer: developing for user interaction. 
 
   Researched answer:
 
 
 2. What is block scope in JavaScript?
 
-  Your answer:
+  Your answer: refers to the accessability of variables. Block scope can only be used by that specific block of code.
 
   Researched answer:
 
 
 3. How would you explain the idea of "inheritance" in object oriented programming?
 
-  Your answer:
+  Your answer: using the same attribute types but adjusting them fro the specific class without retyping it all
 
   Researched answer:
